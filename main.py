ff707709792d9be63927a98965ac3d9d23b10d47
jgaik <jakub.gaik@gmail.com>
2020-04-30 10:39:56 +0200

    Initial commit 	new file:   main.py 	new file:   random/font.ttf

---
+++ main.py
@@ -0,0 +1,107 @@
+import vlc
+import os
+import shutil
+import random
+from time import sleep
+from threading import Thread
+from pynput import keyboard
+from PIL import Image, ImageDraw, ImageFont
+
+
+def getNumbers(path):
+    return [int(x) for x in os.listdir(path) if os.path.isdir(path + os.sep + x)]
+
+
+def randomChoreo(numbers):
+    return random.choices(numbers, k=10)
+
+
+def prepareList(path, choreoNumbers):
+    playlist = []
+    images = []
+    imageList = Image.new('1', (1920, 1080))
+    listDraw = ImageDraw.Draw(imageList)
+    listFont = ImageFont.truetype(path + os.sep + 'font.ttf', 50)
+    listText = "Randomized Body Combat choreography playlist:" + os.linesep
+    for x in range(10):
+        imgTemp = Image.new('1', (1920, 1080))
+        tempDraw = ImageDraw.Draw(imgTemp)
+        trackText = f" -> Track {x} - BodyCombat {choreoNumbers[x]}"
+        tempFont = ImageFont.truetype(path + os.sep + 'font.ttf', 100)
+        tempDraw.text((20, 20), "Next:" + os.linesep +
+                      trackText, fill='white', font=tempFont, spacing=30)
+        listText = listText + trackText + os.linesep
+        file = next(y for y in
+                    os.listdir(path + os.sep + str(choreoNumbers[x])) if y[0] == str(x))
+        playlist.append(os.path.abspath(path) + os.sep +
+                        str(choreoNumbers[x]) + os.sep + file)
+        imgTemp.save(path + os.sep + "temp" + os.sep + f"{x}.png")
+        images.append(path + os.sep + "temp" + os.sep + f"{x}.png")
+    listDraw.text((20, 20), listText, fill='white', font=listFont, spacing=30)
+    imageList.save(path + os.sep + "temp" + os.sep + "list.png")
+    return {'playlist': playlist,
+            'list': path + os.sep + "temp" + os.sep + "list.png",
+            'images': images}
+
+
+class Player:
+
+    def __init__(self, playlist):
+        self.playlist = playlist
+        self.player = vlc.MediaPlayer()
+        self.idx_playing = 0
+        self._listener = keyboard.Listener(on_release=self.thread_key)
+        self._active = False
+        self._playing = False
+
+    def play(self):
+        self._active = True
+        self._listener.start()
+        self.player.set_fullscreen(True)
+        self.player.set_mrl(self.playlist['list'])
+        self.player.play()
+        sleep(1)
+        self.player.pause()
+        while self._active:
+            self.wait()
+            self.player.set_mrl(self.playlist['images'][self.idx_playing])
+            self.player.play()
+            self._playing = False
+            sleep(1)
+            self.player.pause()
+            self.wait()
+            self.player.set_mrl(self.playlist['playlist'][self.idx_playing])
+            self.player.play()
+            while not self.player.get_state() == 6:
+                if not self._active:
+                    break
+            if self.idx_playing < len(self.playlist):
+                self.idx_playing = self.idx_playing + 1
+            else:
+                self._active = False
+        self.player.stop()
+        self.player.release()
+
+    def thread_key(self, key):
+        if key == keyboard.Key.space:
+            self.player.pause()
+            self._playing = not self._playing
+        if key == keyboard.Key.esc:
+            self._active = False
+
+    def wait(self):
+        while not self._playing:
+            if not self._active:
+                return
+
+
+if __name__ == "__main__":
+    path = os.getcwd() + os.sep + "random"
+    try:
+        choreo = randomChoreo(getNumbers(path))
+        os.mkdir(path + os.sep + "temp")
+        playlist = prepareList(path, choreo)
+        player = Player(playlist)
+        player.play()
+    finally:
+        shutil.rmtree(path + os.sep + "temp")
